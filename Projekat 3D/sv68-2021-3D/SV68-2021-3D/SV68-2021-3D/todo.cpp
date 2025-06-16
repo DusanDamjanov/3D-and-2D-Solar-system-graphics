b8fde8c4b6802d581f747323ef94384ae5fb4561
DusanDamjanov <105151463+DusanDamjanov@users.noreply.github.com>
2025-06-16 20:45:30 +0200

    Final

--- Projekat 3D/sv68-2021-3D/SV68-2021-3D/SV68-2021-3D/todo.cpp
+++
@@ -1,9 +0,0 @@
-﻿//TODO: Implementirajte Fongov model osvjetljenja sa suncem kao tackastim osvjetljenjem. [ovo preskoci za sada...imas dole FIXME2 koji objasnjava zasto]
-
-//------------------------------------------------------------------------------------------------------------------------------------------------
- 
-//TODO: Zasto si koristio GLAD umesto GLEW biblioteke i koje su razlike ta dva. I zasto se ne mogu koristiti zajedno?
-
-//FIXME2: ne umem da namestim da se sa normalama tekstura prikazuje kako treba, iz nekog razloga onda tekstura poludi i sjebe se
-
-//FIXME3: teksture se lose pomeraju u skladu sa pomeranjem kamere...to treba resiti....
